976259129bc9a25b3dfbb68147bb254d2ff165df
dvargas42 <dvargas.eng@gmail.com>
2026-06-28 01:03:38 -0300

    feat: incluir imagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Lilian, eu te amo!!!</h1>
+    return (
+        <div>
+            <h1>Lilian, eu te amo!!!</h1>
+            <img style={{width:500 }} src="casal.jpg" alt="casal" />
+        </div>
+    )
 }
 
 export default Home;

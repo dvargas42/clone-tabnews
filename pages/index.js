c8d7f4ccec158b67b2974200868737972391905a
dvargas42 <dvargas.eng@gmail.com>
2026-07-12 13:42:18 -0300

    alterar frase

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home() {
     return (
         <div>
-            <h1>Lilian, eu te amo muito!!!</h1>
+            <h1>Lilian, eu te amo muito!!! 😍</h1>
             <img style={{width:500 }} src="casal.jpg" alt="casal" />
         </div>
     )

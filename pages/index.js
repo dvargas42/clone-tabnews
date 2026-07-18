c8f2ab098a82bc185cdb916d2521ab24be1bf6f2
Daniel Vargas <dvargas.eng@gmail.com>
2026-07-18 21:28:49 +0000

    adicionar scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 function Home() {
-    return (
-        <div>
-            <h1>Lilian, eu te amo muito!!! 😍</h1>
-            <img style={{width:500 }} src="casal.jpg" alt="casal" />
-        </div>
-    )
+  return (
+    <div>
+      <h1>Lilian, eu te amo muito!!! 😍</h1>
+      <img style={{ width: 500 }} src="casal.jpg" alt="casal" />
+    </div>
+  );
 }
 
 export default Home;

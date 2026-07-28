370683587c51b2793d3b0d165444a0eb90afcfca
dvargas42 <dvargas.eng@gmail.com>
2026-07-27 23:17:36 -0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,35 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
+  const databaseName = process.env.POSTGRES_DB;
+  const queries = await Promise.all([
+    database.query("SHOW server_version;"),
+    database.query("SHOW max_connections;"),
+    database.query({
+      text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+      values: [databaseName],
+    }),
+  ]);
+
+  const resultVersion = queries[0].rows[0].server_version;
+  const resultMaxConnections = queries[1].rows[0].max_connections;
+  const resultOpennedConnections = queries[2].rows[0].count;
+
+  const updatedAt = new Date().toISOString();
   response.status(200).json({
-    chave: "sao acima da media",
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: resultVersion,
+        max_connections: parseInt(resultMaxConnections),
+        opened_connections: resultOpennedConnections,
+      },
+    },
   });
 }
 
 export default status;
+
+// versao do Postgres
+// conexoes maximas
+// conexoes usada
--- test/integration/api/v1/status/get.test.js
+++ test/integration/api/v1/status/get.test.js
@@ -1,4 +1,16 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  //protege contra null que convertido torna-se "1970-01-01T00:00:00.000Z"
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  const database = responseBody.dependencies.database;
+  expect(database.version).toBe("16.0");
+  expect(database.max_connections).toBe(100);
+  expect(database.opened_connections).toEqual(1);
 });

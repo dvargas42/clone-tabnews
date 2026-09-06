8e1dee3947ca2ddd30370f07e056153763c19f9e
dvargas42 <dvargas.eng@gmail.com>
2026-09-06 18:56:44 -0300

    makes npm test more robust with async-retry and orchestrator.js

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,7 @@ const createJestConfig = nextJest({
 });
 const jestConfig = createJestConfig({
   moduleDirectories: ["node_modules", "<rootDir>"],
+  testTimeout: 60000, //60s
 });
 
 module.exports = jestConfig;
